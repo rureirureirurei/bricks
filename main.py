8fa8c94b3f5d1027c8f2da11677e006ee9b48fe8
Denys <denys.zinoviev@gmail.com>
2022-10-02 22:18:23 +0200

    Imroved perfomance, minor syntax improvements

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import os
 import random
 import sys
 
-
-WIDTH = 800
-HEIGHT = 800
-
 NOICE_KERNEL_SIZE = (9, 9)
 ADAPTIVE_THRESHOLD_KERNEL_SIZE = 21
 ADAPTIVE_THRESHOLD_CONSTANT = 3
-CONTOUR_AREA_THRESHOLD = 250
-CONTOUR_AREA_COUNTING_THRESHOLD = 50
+CONTOUR_AREA_THRESHOLD = 150
+CONTOUR_AREA_COUNTING_THRESHOLD = 250
 CONTOURS_DELTA = 150
 EROSION_KERNEL = np.full((15,15), -5, np.uint8)
 EROSION_KERNEL_SMALL_PIECES = np.full((3,3), -5, np.uint8)
@@ -45,10 +41,9 @@ if __name__ == "__main__":
     images_amount = len(images)
 
     for index in range(images_amount):
-        img = images[index]
+        image = images[index]
         name = names[index]
-        shrinked = cv.resize(img, (WIDTH, HEIGHT))
-        blurred = cv.GaussianBlur(shrinked, (NOICE_KERNEL_SIZE), 0)
+        blurred = cv.GaussianBlur(image, (NOICE_KERNEL_SIZE), 0)
         grayscaled = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
         thresh = cv.adaptiveThreshold(grayscaled,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,ADAPTIVE_THRESHOLD_KERNEL_SIZE,ADAPTIVE_THRESHOLD_CONSTANT)
         thresh_inversed = cv.bitwise_not(thresh)
@@ -65,16 +60,17 @@ if __name__ == "__main__":
             eroded = cv.erode(thresh, EROSION_KERNEL_SMALL_PIECES)
             contours = find_filtered_contours(eroded, 2)
 
-        result = img.copy()
+        result = image.copy()
         print(len(contours))
         for i in contours:
             M = cv.moments(i)
             if M['m00'] != 0:
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
-                img_width = np.shape(img)[0]
-                img_height = np.shape(img)[1]
-                cv.circle(result, (int(cx * img_height / HEIGHT), int(cy * img_width / WIDTH)), 3, (0, 0, 255), -1) #TODO
+                image_width = np.shape(image)[0]
+                image_height = np.shape(image)[1]
+                cv.circle(result, (cx, cy), 3, (0, 0, 255), -1) #TODO
+
 
         cv.putText(result, str(len(contours)), (50,50), cv.FONT_HERSHEY_SIMPLEX, 1, (240, 255, 255), 2, cv.LINE_AA)
         cv.imwrite('./res/'+name, result)
